bf45ab2eb6ed689a03e318339f5cf07fea295d04
jwp8237 <jwp6281@gmail.com>
2021-10-26 16:18:00 +0900

    Daum News 목록 -> 제목, 본문 수집

--- practice/DaumNewsOne.py
+++ practice/DaumNewsOne.py
@@ -8,12 +8,21 @@ from bs4 import BeautifulSoup
 # BeautifulSoup4 => requests GET 해온 코드에서 필요한 정보만 find 해서 찾아오기
 
 url = 'https://news.v.daum.net/v/20211021152915953'
-
 result = requests.get(url)
 
 doc = BeautifulSoup(result.text, 'html.parser')
+title = doc.select('h3.tit_view')[0].get_text()
+contents = doc.select('section > p')
 
-title = doc.select('h3.tit_view')
+contents.pop(-1)
+content = '' # 본문 총합
+for info in contents:
+    content += info.get_text()
+print('#################################')
+print('# 뉴스 제목: {}'.format(title))
+print('#################################')
+print('# 뉴스 본문: {}'.format(content))
 
-print('# 뉴스 제목: {}'.format(title[0].get_text()))
+
+
 
